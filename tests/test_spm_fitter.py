2fcbf99588462efe9df9525d36f78ed1ef0edc3f
Robert Jensen <jensen.robert@gmail.com>
2024-06-28 11:20:30 +0200

    More tests, coverage is now 35%
    Coverage will never be 100 since the plotting code will most likely
    never be included in these tests.

--- tests/test_spm_fitter.py
+++ tests/test_spm_fitter.py
@@ -24,6 +24,39 @@ class TestSPMFitterFitter(unittest.TestCase):
         self.assertAlmostEqual(x_size, 1.20702e-5, delta=1e-9)
         self.assertAlmostEqual(y_size, 1.20702e-5, delta=1e-9)
 
+    def test_find_modulated_lines(self):
+        """
+        Test that the auto-detection of modulated lines works.
+        Expected result is to find all lines from 111 to 483
+        """
+        modulated_lines = self.fitter._find_modulated_lines()
+        for line in range(111, 483):
+            self.assertTrue(line in modulated_lines)
+
+    def test_index_to_area(self):
+        # Coordinates to test index-to-area. Happens to also be the
+        # coordinates of the patterned area
+        coords = {'x_l': 27, 'x_r': 469, 'y_t': 88, 'y_b': 507}
+        area = self.fitter._index_to_area(**coords)
+        expected_area = (
+            (0.6365197265624999, 0.11787402343749999),
+            (11.056583398437498, 9.995717187499999)
+        )
+        self.assertAlmostEqual(area[0][0], expected_area[0][0], delta=1e-9)
+        self.assertAlmostEqual(area[0][1], expected_area[0][1], delta=1e-9)
+        self.assertAlmostEqual(area[1][0], expected_area[1][0], delta=1e-9)
+        self.assertAlmostEqual(area[1][1], expected_area[1][1], delta=1e-9)
+
+    def test_hat_line_fit(self):
+        # Test that we cannot fit a hat to line 111
+        hat_fit = self.fitter._fit_hat_to_line(line_nr=111, plot=False)
+        self.assertIsNone(hat_fit)
+
+        # Test that we can fit a hat to line 112
+        hat_fit = self.fitter._fit_hat_to_line(line_nr=112, plot=False)
+        expected_fit = (86, 410, 112)
+        self.assertEqual(hat_fit, expected_fit)
+
     def test_full_image_roughness(self):
         """
         Calculate the roughness of the entire image.
@@ -38,7 +71,6 @@ class TestSPMFitterFitter(unittest.TestCase):
         Also need to test fit to sub-area as well as a fit to a masked area
         """
         z = self.fitter._plane_fit(plot=False)
-
         self.assertEqual(z.shape, (512, 512))
         self.assertAlmostEqual(sum(z[0][:]), -0.00033277827779927305, delta=1e-14)
         self.assertAlmostEqual(z[0][0], -6.419494146295475e-07, delta=1e-14)
