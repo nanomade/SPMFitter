9135db4d65309849a4a529c4812d2e61f77b1edf
Robert Jensen <jensen.robert@gmail.com>
2024-10-03 14:11:00 +0200

    Also remove the actual top-hat function (that is now unused).

--- fit_functions.py
+++ fit_functions.py
@@ -8,16 +8,3 @@ def sine_error_func(p, x, y):
     error = sine_fit_func(p, x) - y
     return error
 
-def top_hat(p, x, line):
-    # p[0]: End of first low part of hat
-    # p[1]: Start of second part of hat
-    # p[2]: z-value of low part of 
-    # p[3]: z-value of high part of 
-    low = np.append(np.where(x < p[0])[0], np.where(x > p[1])[0])
-    value = np.zeros(len(line)) + p[3]
-    np.put(value, low, p[2])
-    return value
-
-def top_hat_error_func(p, x, y):
-    error = top_hat(p, x, line=y) - y
-    return error
